d72ffd45118119aaa7760bdb3cbb723d98605c6e
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-05-31 20:44:51 +0000

    Some copy/pasting from example code as instructed, new partials and changes to feed and users/show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+    # Given a full URL string, return just the host + path
+  def only_host_and_path(raw_url)
+    return "" if raw_url.blank?
+
+    uri = URI.parse(raw_url)
+    host_and_path = "#{uri.host}#{uri.path}"
+    return host_and_path
+  rescue URI::InvalidURIError
+    # if it wasn’t a valid URL, just return whatever they entered
+    return raw_url
+  end
 end
