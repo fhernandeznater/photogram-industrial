eb47b21e0f68f527ad5821fc601f1974f898f1d7
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-05-31 18:06:15 +0000

    some more Devise work! Guess I hadn't looked ahead at the hints part of the lesson

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,8 +9,8 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar_image])
-    devise_parameter_sanitizer.permit(:account_update, keys: [ :username, :avatar_image ])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [ :username, :private, :name, :bio, :website, :avatar_image ])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :username, :private, :name, :bio, :website, :avatar_image ])
 
   end
 
