65615e42e5272e2ab0a62b609a008485c2bdfcfb
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-05-31 17:26:52 +0000

    Used Study Buddy to help with modifying the application controller to whitelist username and avatar image parameters that the grade tests require for a new signup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,17 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!
+  # only require login on *non*-Devise controllers
+  before_action :authenticate_user!, unless: :devise_controller?
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :avatar_image])
+    devise_parameter_sanitizer.permit(:account_update, keys: [ :username, :avatar_image ])
+
+  end
+
 end
