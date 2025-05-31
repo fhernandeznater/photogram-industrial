860a3eecc5502a1f83e2f8e384a2854143b162bb
Federico Hernandez Nater <166563763+fhernandeznater@users.noreply.github.com>
2025-05-31 15:32:30 +0000

    Set up users#show action

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,8 @@ Rails.application.routes.draw do
 
   root "photos#index"
 
+  get "/:username" => "users#show", as: :user
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -19,4 +21,3 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 end
-
